b02ba0226d3ad027bff7bea4da0bf1d9fabd6450
antoine lafitte <antoine.lafitte99411@gmail.com>
2025-03-05 18:09:01 +0100

    Added React frontend

--- SkateBoarding-Platform/migrations/versions/b393448c6287_initial_migration_for_main_branch.py
+++ SkateBoarding-Platform/migrations/versions/b393448c6287_initial_migration_for_main_branch.py
@@ -1,8 +1,8 @@
-"""Initial migration
+"""Initial migration for main branch
 
-Revision ID: e50cac431b4f
+Revision ID: b393448c6287
 Revises: 
-Create Date: 2025-03-04 15:24:42.345910
+Create Date: 2025-03-05 15:02:48.460159
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e50cac431b4f'
+revision = 'b393448c6287'
 down_revision = None
 branch_labels = None
 depends_on = None
