a51531ce3f8372962b996710ba2d39126b763b76
antoine lafitte <antoine.lafitte99411@gmail.com>
2025-03-07 18:48:01 +0100

    Added Tailwind config files, giving up on tailwind not working

--- skateboarding-platform/src/main.jsx
+++ skateboarding-platform/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx'; //  create this next
 import Home from './pages/Home.jsx';
+import TestTailwind from './pages/TestTailwind';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/test-tailwind" element={<TestTailwind />} />
                 {/* Add more routes here later */}
             </Routes>
         </Router>
---
+++ skateboarding-platform/src/pages/TestTailwind.jsx
@@ -0,0 +1,11 @@
+const TestTailwind = () => {
+  return (
+      <div className="flex justify-center items-center h-screen bg-gray-200 !important">
+          <h1 className="text-3xl font-bold text-blue-500 !important">
+              Tailwind is working! 🎉
+          </h1>
+      </div>
+  );
+};
+
+export default TestTailwind;
