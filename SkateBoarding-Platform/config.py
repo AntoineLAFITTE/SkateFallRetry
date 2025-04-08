a4d7f6057290a02bac2a7cbea925452fb73f39ec
antoine lafitte <antoine.lafitte99411@gmail.com>
2025-04-08 18:14:22 +0200

    Postgres setup

--- SkateBoarding-Platform/config.py
+++ SkateBoarding-Platform/config.py
@@ -4,6 +4,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get absolute path of pr
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'skateboarding_platform.db')}"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///skateboarding_platform.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
