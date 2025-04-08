1e7d1f5667d4b51fd3d6a1bb72d38b002082cbb2
antoine lafitte <antoine.lafitte99411@gmail.com>
2025-04-08 19:28:38 +0200

    Updated config file to support Render PostgreSQl

--- SkateBoarding-Platform/config.py
+++ SkateBoarding-Platform/config.py
@@ -1,9 +1,14 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get absolute path of project
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///skateboarding_platform.db')
+
+    raw_db_url = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'skateboarding_platform.db')}")
+
+    # Fix Render's postgres:// to postgresql:// for SQLAlchemy compatibility
+    SQLALCHEMY_DATABASE_URI = raw_db_url.replace("postgres://", "postgresql://")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
