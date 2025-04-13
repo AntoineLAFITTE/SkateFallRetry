36d3c341a5478e5b7d778940c79745b82d93ac2c
antoine lafitte <antoine.lafitte99411@gmail.com>
2025-04-13 16:29:47 +0200

    Acess token type str to comply with JWT

--- SkateBoarding-Platform/routes/auth.py
+++ SkateBoarding-Platform/routes/auth.py
@@ -36,5 +36,5 @@ def login():
     if not user or not user.check_password(password):
         return jsonify({'error': 'Invalid credentials'}), 401
 
-    access_token = create_access_token(identity=user.id)  # Generate JWT token
+    access_token = create_access_token(identity=str(user.id))  # Generate JWT token
     return jsonify({'message': 'Login successful', 'access_token': access_token}), 200
